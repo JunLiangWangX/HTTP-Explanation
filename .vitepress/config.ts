0b841afb7df05bc26b366c0a924358b2a617b2d7
汪军梁 <1428637884@qq.com>
2023-06-21 16:01:49 +0800

    init project

---
+++ .vitepress/config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "HTTP完全注释",
+  description: "HTTP完全注释",
+  lang:"zh-CN",
+  base: '/HTTP-Comment/',
+  srcExclude: ['**/README.md', '**/TODO.md'],
+  outDir: './public',
+  lastUpdated:true,
+  head: [
+    [
+      'link',
+      { rel: 'icon', href: './.vitepress/assets/logo.svg'}
+    ]
+  ],
+  themeConfig: {
+    // logo:'./.vitepress/assets/logo.svg',
+    siteTitle:'HTTP完全注解',
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ index.md
@@ -0,0 +1,18 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "HTTP完全注释"
+  text: ""
+  tagline: 全面、集中、准确
+  actions:
+    - theme: brand
+      text: 开始阅读
+      link: /markdown-examples
+    - theme: alt
+      text: 访问Github
+      link: https://github.com/vuejs/vitepress
+
+---
+
