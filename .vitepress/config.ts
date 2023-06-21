55e5eae38268199c43f17260ba2fcd41a930a1cb
汪军梁 <1428637884@qq.com>
2023-06-21 17:06:59 +0800

    style:add logo

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   head: [
     [
       'link',
-      { rel: 'icon', href: './.vitepress/assets/logo.svg'}
+      { rel: 'icon', href: '/logo.svg'}
     ]
   ],
   themeConfig: {
