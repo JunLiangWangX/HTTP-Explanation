439c70f7bbe32d431b49c71ea647999df33c0668
wangjl <1428637884@qq.com>
2023-06-23 11:09:18 +0800

    chore:complete config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -13,12 +13,26 @@ export default defineConfig({
   head: [
     [
       'link',
-      { rel: 'icon', href: '/logo.svg'}
+      { rel: 'icon', href: './logo.svg'}
     ]
   ],
   themeConfig: {
     // logo:'./.vitepress/assets/logo.svg',
     siteTitle:'HTTP完全注解',
+    lastUpdatedText:'最后更新',
+    darkModeSwitchLabel:'切换模式',
+    sidebarMenuLabel:"菜单",
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
+    returnToTopLabel:'返回顶部',
+    langMenuLabel:'切换语言',
+    outlineTitle:'目录',
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2023-present JunLiangWang'
+    },
     sidebar: [
       {
         text: 'Examples',
@@ -30,7 +44,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/JunLiangWangX/HTTP-Explanation/'}
     ]
   }
 })
--- src/index.md
+++ src/index.md
@@ -1,7 +1,7 @@
 ---
 # https://vitepress.dev/reference/default-theme-home-page
 layout: home
-
+title: 首页
 hero:
   name: "HTTP完全注释"
   text: ""
