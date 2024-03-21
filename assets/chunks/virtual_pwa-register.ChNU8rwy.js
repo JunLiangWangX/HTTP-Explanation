c82dbab6761057df411fdf755c73b4cd1b7aac2d
JunLiangWangX <JunLiangWangX@users.noreply.github.com>
2024-03-21 02:19:08 +0000

    deploy: 92a6ef0ae3f69758230d7fcdd2b4a8d738ed40a7

---
+++ assets/chunks/virtual_pwa-register.ChNU8rwy.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{Y as x}from"./framework.CM4sUzcX.js";function P(w={}){const{immediate:E=!1,onNeedRefresh:r,onOfflineReady:t,onRegistered:n,onRegisteredSW:d,onRegisterError:o}=w;let e,c,s;const g=async(p=!0)=>{await c,await(s==null?void 0:s())};async function u(){if("serviceWorker"in navigator){const{Workbox:p}=await x(()=>import("./workbox-window.prod.es5.DFjpnwFp.js"),__vite__mapDeps([]));e=new p("/HTTP-Explanation/sw.js",{scope:"/HTTP-Explanation/",type:"classic"}),s=async()=>{await(e==null?void 0:e.messageSkipWaiting())};{let i=!1;const l=()=>{i=!0,e==null||e.addEventListener("controlling",a=>{a.isUpdate&&window.location.reload()}),r==null||r()};e.addEventListener("installed",a=>{typeof a.isUpdate>"u"?typeof a.isExternal<"u"?a.isExternal?l():!i&&(t==null||t()):a.isExternal?window.location.reload():!i&&(t==null||t()):a.isUpdate||t==null||t()}),e.addEventListener("waiting",l),e.addEventListener("externalwaiting",l)}e.register({immediate:E}).then(i=>{d?d("/HTTP-Explanation/sw.js",i):n==null||n(i)}).catch(i=>{o==null||o(i)})}}return c=u(),g}export{P as registerSW};
